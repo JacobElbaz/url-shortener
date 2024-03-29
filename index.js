aa2d1b06daa6a5e0b6e147479453bb179562b3ac
JacobElbaz <jacobel@post.bgu.ac.il>
2024-03-29 13:07:33 +0300

    fix

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+let urlArray = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const urlRegex = /^(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  if (!urlRegex.test(req.body.url)) return res.json({ error: "invalid url" });
+  urlArray.push(req.body.url);
+  res.json({ original_url: req.body.url, short_url: urlArray.length });
+});
+
+app.get("/api/shorturl/:url", (req, res) => {
+  const url = parseInt(req.params.url);
+  res.redirect(urlArray[url - 1]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
